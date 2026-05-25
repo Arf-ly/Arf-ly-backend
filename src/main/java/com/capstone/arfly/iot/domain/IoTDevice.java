8f7b9c46748c8516727003a2f37e32e6cc6987d4
SHNAME <tlgud119@gmail.com>
2026-05-25 15:38:25 +0900

    feat(domain): ioT 장치 연결 및 산책 기록 저장을 위한 테이블 생성

---
+++ src/main/java/com/capstone/arfly/iot/domain/IoTDevice.java
@@ -0,0 +1,37 @@
+package com.capstone.arfly.iot.domain;
+
+import com.capstone.arfly.common.domain.BaseTimeEntity;
+import com.capstone.arfly.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class IoTDevice extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false, unique = true)
+    private String deviceUid;
+
+}
---
+++ src/main/java/com/capstone/arfly/walkRecord/domain/WalkRecord.java
@@ -0,0 +1,59 @@
+package com.capstone.arfly.walkRecord.domain;
+
+import com.capstone.arfly.common.domain.BaseCreatedEntity;
+import com.capstone.arfly.iot.domain.IoTDevice;
+import com.capstone.arfly.member.domain.Member;
+import com.capstone.arfly.pet.domain.Pet;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class WalkRecord extends BaseCreatedEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+    @Column(nullable = false)
+    private LocalDateTime endTime;
+
+    @Column(nullable = false)
+    private Double activityScore;
+
+    @Column(nullable = false)
+    private Double totalDistance;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean status = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "device_id", nullable = false)
+    private IoTDevice device;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+}
