32594e034c2e0f67fbb865f5cf7996f871db01a9
SHNAME <tlgud119@gmail.com>
2026-09-03 16:00:07 +0900

    fix: 로컬 프로필 설정이 .env 값에 밀리는 문제 수정

--- src/main/java/com/capstone/arfly/ArfLyApplication.java
+++ src/main/java/com/capstone/arfly/ArfLyApplication.java
@@ -1,5 +1,5 @@
 package com.capstone.arfly;
-import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,14 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ArfLyApplication {
 
     public static void main(String[] args) {
-        // 파일이 없어도 에러를 내지 않고 무시하도록 설정 (로컬 개발 .env를 위해서)
-        Dotenv dotenv = Dotenv.configure()
-                .ignoreIfMissing()
-                .load();
-
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
         SpringApplication.run(ArfLyApplication.class, args);
     }
-
 }
